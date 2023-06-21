02297de6fd81a0972827966572938429f0f7a248
db yaga <charlesyagazie95@gmail.com>
2023-06-21 19:18:12 +0100

    active nav

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,11 +2,7 @@ const nav = document.querySelector('nav');
 const header = document.querySelector('header');
 
 const navObserverCallback = (watchEntry, navObserver) => {
-	if (!watchEntry[0].isIntersecting) {
-		nav.classList.add('active');
-	} else {
-		nav.classList.remove('active');
-	}
+	if (watchEntry[0].isIntersecting)
 }
 
 const navObserverOptions = {

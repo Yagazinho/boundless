84d8929d57d331ee2b153a5a0a6ee5091feaf5a0
db yaga <charlesyagazie95@gmail.com>
2023-06-23 01:45:54 +0100

    something went wrong

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,12 +2,17 @@ const nav = document.querySelector('nav');
 const header = document.querySelector('header');
 
 const navObserverCallback = (watchEntry, navObserver) => {
-	if (watchEntry[0].isIntersecting)
+
+	if (!watchEntry[0].isIntersecting) {
+		nav.classList.add('active');
+	} else {
+		nav.classList.remove('active');
+	}
 }
 
 const navObserverOptions = {
-	rootMargin: '-9px',
-	threshold: 0
+	rootMargin: '0px 0px 10px 0px',
+	threshold: .1
 }
 
 const navObserver = new IntersectionObserver(navObserverCallback, navObserverOptions);
